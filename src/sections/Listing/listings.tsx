040bd3b001f7c74911315f96cba6228cabe6369f
vbybyk <vitaliy.bybyk@gmail.com>
2022-05-26 15:00:21 +0300

    create: deleteListings using GraphQL

--- src/sections/Listing/listings.tsx
+++ src/sections/Listing/listings.tsx
@@ -1,4 +1,5 @@
 import {server} from '../../lib'
+import {ListingsData, DeleteListingData, DeleteListingVariables} from './types'
 
 interface Props {
     title: string;
@@ -20,10 +21,27 @@ const LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`
+
 export const Listings = ({title} : Props) => {
 
   const fetchListings = async () => {
-    const {data} = await server.fetch({ query : LISTINGS})
+    const {data} = await server.fetch<ListingsData>({ query : LISTINGS})
+    console.log(data)
+  }
+
+  const deleteListings = async () => {
+    const {data} = await server.fetch<DeleteListingData, DeleteListingVariables>({ 
+      query : DELETE_LISTING,
+      variables : {
+        id: '628d0740bb66f768127d42cf'}
+      })
     console.log(data)
   }
 
@@ -32,6 +50,7 @@ export const Listings = ({title} : Props) => {
     <div className="Listings">
         <h1>{title}</h1>
         <button onClick={fetchListings}>Fetch listings</button>
+        <button onClick={deleteListings}>Delete listing</button>
     </div>
   );
 }
