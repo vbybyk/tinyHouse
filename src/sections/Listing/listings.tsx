be64ab00fc0c53fb1c3d1fed77cdfc15673f535d
vbybyk <vitaliy.bybyk@gmail.com>
2022-05-26 13:20:49 +0300

    create:server.ts,query Listings using GraphQL serv

--- src/sections/Listing/listings.tsx
+++ src/sections/Listing/listings.tsx
@@ -1,12 +1,37 @@
+import {server} from '../../lib'
+
 interface Props {
     title: string;
+}
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
   }
+`;
 
 export const Listings = ({title} : Props) => {
-  
+
+  const fetchListings = async () => {
+    const {data} = await server.fetch({ query : LISTINGS})
+    console.log(data)
+  }
+
+
   return (
     <div className="Listings">
         <h1>{title}</h1>
+        <button onClick={fetchListings}>Fetch listings</button>
     </div>
   );
 }
