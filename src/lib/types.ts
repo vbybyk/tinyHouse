8f9c423ebeca0394eaa1937262e7d3dd4cebca2e
vbybyk <vitaliy.bybyk@gmail.com>
2022-05-30 16:18:56 +0300

    add: types.ts all types and interfaces

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,23 +1,56 @@
 import { Collection, ObjectId } from "mongodb";
 
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE"
+}
+
+interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+
+interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
+}
+
 export interface Booking {
   _id: ObjectId;
+  listing: ObjectId;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
 }
 
 export interface User {
   _id: ObjectId;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  income: number;
+  bookings: ObjectId[];
+  listings: ObjectId[];
 }
 
 export interface Listing {
   _id: ObjectId;
-  // title: string;
-  // image: string;
-  // address: string;
-  // price: number;
-  // numOfGuests: number;
-  // numOfBeds: number;
-  // numOfBaths: number;
-  // rating: number;
+  title: string;
+  description: string;
+  image: string;
+  host: string;
+  type: ListingType;
+  address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndex;
+  price: number;
 }
 
 export interface Database {
